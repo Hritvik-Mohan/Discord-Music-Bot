d940abcd9713680bff6cb958e0335a1befd6fa0d
Hritvik-Mohan <hritvik5005@gmail.com>
2021-11-01 22:28:59 +0530

    Made some changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.all("/", (req, res) => {
 })
 
 function keepAlive() {
-  server.listen(3000, () => {
+  server.listen(process.env.PORT || 5000, () => {
     console.log("Server is ready.")
   })
 }
