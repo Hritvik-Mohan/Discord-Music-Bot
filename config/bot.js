2370d7a61fa1e68e4c89c9339738d3b43f296fd0
Hritvik-Mohan <hritvik5005@gmail.com>
2021-11-01 22:21:16 +0530

    Made some changes

--- config/bot.js
+++ config/bot.js
@@ -10,7 +10,7 @@ module.exports = {
     discord: {
         token: process.env.TOKEN,
         prefix: '*',
-        activity: 'Chill with Mona',
+        activity: 'Chill with Mona!',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
